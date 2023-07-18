681b7a1f6966b28870ab1647582e5b502bf88521
06AlexS2 <sanchez.alejandro0603@gmail.com>
2023-07-18 09:44:36 -0600

    first commit for the about feature

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   };
 export default function About() {
     return (
-        <main className="flex min-h-screen min-w-screen flex-col items-center justify-between bg-gray-800">
+        <main className="flex h-[100vh] w-[100vw] flex-col items-center justify-between bg-gray-800">
             <h1>This is gonna be the about section.</h1>
         </main>
     )
